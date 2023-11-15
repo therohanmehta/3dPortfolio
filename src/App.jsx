d3be6db3409a0490ebccc915e36357945d460a04
therohanmehta <rohandhn786@gmail.com>
2023-11-15 20:24:21 +0530

    Rohan - added the model fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import { Suspense } from "react";
 function App() {
   return (
     <>
-      <Canvas shadows camera={{ position: [0,10,3], fov: 30 }}>
+      <Canvas shadows camera={{ position: [0,0,10], fov: 30 }}>
         <color attach="background" args={["#211d1d"]} />
-        <Experience />
+        <Experience  />
         <Suspense>
          
           <Stars />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, ScrollControls } from "@react-three/drei";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
 import { Rohan } from "./RohanModel";
 
 function Experience() {
@@ -6,11 +6,13 @@ function Experience() {
     <>
       <OrbitControls enableRotate />
 {/* <ScrollControls pages={3}/> */}
-      <group position-y={-1}>
+      <group position-y={-2.5} scale={2} >
         <Rohan />
       </group>
-
+      {/* <directionalLight position={[100, 200,500]} color="white" intensity={10} /> */}
+      {/* <Environment preset="Sunset" /> */}
       <ambientLight intensity={2} />
+      
     </>
   );
 }
